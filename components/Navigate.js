c69971f60ab85e2a0a30d89369d7020878a3c529
Ida Laxmi <158257619+Ida12Laxmi@users.noreply.github.com>
2026-02-18 11:42:54 +0545

    Update Navigate.js

--- components/Navigate.js
+++ components/Navigate.js
@@ -2,6 +2,7 @@ import React, { useContext} from "react";
 import {Link, useNavigate} from "react-router-dom";
 import { AuthContext } from "./AuthContext";
 import "./Navigate.css";
+import logo from "./react/logo.png";
 
 function Navbar(){
     const {user,logout}=useContext(AuthContext);
@@ -12,9 +13,30 @@ function Navbar(){
     navigate("/login");
    };
    return(
-       <nav className="navbar">
-               <h2 className="logo" onClick={() => navigate("/")} style={{cursor: 'pointer'}}>Buy Dress</h2>
+       <nav className="navbar" style={{ padding: "10px", background: "#333", color: "#fff", display: "flex", justifyContent: "space-between" }}>
+        <div className="logo-container" onClick={() => navigate("/")}>
+            <div className="logo-circle">
+            <img src={logo} alt="Boutique Logo" className="logo-img" />
+            </div>
+               <h2 className="logo"  style={{cursor: 'pointer'}}>Buy Dress</h2>
+               </div>
                <ul className="nav-links">
+               <div>
+                {/* Visual Check: This will show you exactly who is logged in */}
+                {user ? (
+                    <span>Welcome, <b>{user.cus_name}</b> </span>
+                ) : (
+                    <span></span>
+                )}
+            </div>
+            {user && (
+                    <li><Link to="/Chatbot">ChatWithAI</Link></li>
+                  
+                )}
+            {user && (
+                    <li><Link to="/Notifications">Notifications</Link></li>
+                  
+                )} 
                 
                 {user && (
                     <li><Link to="/MyOrders">My Orders</Link></li>
@@ -24,6 +46,11 @@ function Navbar(){
                     
                     <li><Link to="/DressDetails">Dresses</Link></li>
                 )}
+                 {user && (
+                    
+                    <li><Link to="/AboutUs">AboutUS</Link></li>
+                )}
+
                 {!user ? (
                      <>
                      <li><Link to="/login">Login</Link></li>
